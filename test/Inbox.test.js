05af5a5c606c8fa5f1c524767862c40bd09b6c2b
adolaher <addolaher@gmail.com>
2022-01-09 16:05:44 -0500

    Start testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,18 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  });
+// Use account to deploy contract
+
+describe('Inbox', () => {
+  it('deploy contract', () => {
+    console.log(accounts);
+  });
+});
