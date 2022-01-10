de8fd83e4e7c4bd93c7ae53a52d6620afbbfc0e7
adolaher <addolaher@gmail.com>
2022-01-09 20:25:03 -0500

    Setup deploy script + mnemonic phrase env_variable

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {interface, bytecode} = require('./compile')
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC_PHRASE,
+  'https://rinkeby.infura.io/v3/ac644ddf4c1145c79e6c4fd564e5a60e'
+);
+const web3 = new Web3(provider);
